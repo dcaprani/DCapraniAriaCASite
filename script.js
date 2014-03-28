11d33f2b5bb531169aca9a58d3ad6a7f350df8df
dcaprani <dcaprani@me.com>
2014-03-28 11:40:47 +0000

    Nearly There
    App working introduced media query to css

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 			function convert()
 			{
 				var input = document.getElementById("toBeConverted").value;
-				var centi2In = input*3;
-				var in2Cent = input/3;
-				var centg2Fahr = input*9;
-				var fahr2Cent = input/9;
+				var centi2In = input*.39370;
+				centi2In = centi2In.toFixed(2);
+				var in2Cent = input*2.54;
+				in2Cent = in2Cent.toFixed(2); 
+				var centg2Fahr = ((input*9)/5)+32;
+				centg2Fahr = centg2Fahr.toFixed(2);
+				var fahr2Cent = (input-32) *(5/9);
+				fahr2Cent = fahr2Cent.toFixed(2);
 				
 				document.getElementById("Centi2In").innerHTML = input +" cms = " + centi2In + " ins";
 				document.getElementById("In2Centm").innerHTML = input +" ins = " + in2Cent + " cms";
